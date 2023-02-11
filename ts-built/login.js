7726daa7079b5f4fe8772533c3aae865b62be8c2
azizkale <azizkale@hotmail.com>
2023-02-11 21:38:00 +0100

    .

--- ts-built/login.js
+++ ts-built/login.js
@@ -1,5 +1,5 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.login = void 0;
 var login = function () {
     window.location.replace("./views/group.html");
