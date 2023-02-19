cfe7c1e5e88c292af45cbbbe1d5492887d813a52
azizkale <azizkale@hotmail.com>
2023-02-19 11:10:59 +0100

    .

---
+++ src/tools/secretkey.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.secretkey = void 0;
+exports.secretkey = "BCWolGeVK1LKXZLCTQoiOhoUDZvbsUizOFrNJW5q944RyHyhKxQhwPZtTDsFnfDqi2sPuFr8Gt-Gk9MkvfmKHq4";
