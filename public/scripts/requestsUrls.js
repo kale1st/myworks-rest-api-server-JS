15c4bc99c30c37bc0fb4d58a5752b158c79f5213
kale1st <59444395+kale1st@users.noreply.github.com>
2023-03-11 12:17:55 +0100

    Delete public directory

--- public/scripts/requestsUrls.js
+++
@@ -1,6 +0,0 @@
-const mainUrl = "http://localhost:3000"
-    // const mainUrl = "http://192.168.0.17:3000"
-const reqJSONData = {
-    req_signin: mainUrl + "/signin",
-    req_register: mainUrl + '/users/createuser'
-}
