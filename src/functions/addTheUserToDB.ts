b0a153656e043a9f06ffe949709206d9fe9f6444
azizkale <azizkale@hotmail.com>
2023-03-11 13:58:10 +0100

    .

--- src/functions/addTheUserToDB.ts
+++ src/functions/addTheUserToDB.ts
@@ -2,8 +2,7 @@ import { getDatabase, ref, set } from "firebase/database";
 import { Request, Response } from 'express';
 
 export const addTheUserToDB = async (req: Request, res: Response) => {
-    const { email } = await req.body;
-    const writeUserData = (userId, name, email, imageUrl) => {
+    (userId, name, email, imageUrl) => {
         const db = getDatabase();
         set(ref(db, 'users/' + userId), {
             username: name,
