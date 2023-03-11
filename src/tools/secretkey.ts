3d45531e6ef070fb435f2bf1b154d2cd88f69408
azizkale <azizkale@hotmail.com>
2023-03-11 12:10:30 +0100

    .

--- src/tools/secretkey.ts
+++ src/tools/secretkey.ts
@@ -1,1 +1,1 @@
-export const secretkey = "BCWolGeVK1LKXZLCTQoiOhoUDZvbsUizOFrNJW5q944RyHyhKxQhwPZtTDsFnfDqi2sPuFr8Gt-Gk9MkvfmKHq4";
+// export const secretkey = "BCWolGeVK1LKXZLCTQoiOhoUDZvbsUizOFrNJW5q944RyHyhKxQhwPZtTDsFnfDqi2sPuFr8Gt-Gk9MkvfmKHq4";
