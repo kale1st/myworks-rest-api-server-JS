953a53832e1572c1859e18100e5e72b4c02b4541
azizkale <azizkale@hotmail.com>
2023-04-05 10:12:07 +0200

    fix/update user info fixed

--- src/routes/settings/settingscontrollers.ts
+++ src/routes/settings/settingscontrollers.ts
@@ -5,31 +5,31 @@ const auth = getAuth();
 
 const getUserInfo = async (req: Request, res: Response) => {
     const token = req.headers['authorization'].split(' ')[1];
-    const auth = getAuth();
-    const user = auth.currentUser;
     await admin.auth().verifyIdToken(token).then(async (response) => {
-        //
-        if (user) {
-            return res.status(200).send(user)
-        }
+        if (response)
+            admin.auth().getUserByEmail(response.email).then((user) => {
+                return res.status(200).send(user)
+            })
         else
-            return res.status(401).send();
+            return res.status(404).send('user not found')
+
     })
 }
 const updateUser = async (req: Request, res: Response) => {
     const { updateObject } = req.body
-
-    updateProfile(auth.currentUser, {
-        photoURL: updateObject.photoURL,
-        displayName: updateObject.displayName,
-    }).then(() => {
-        console.log('profile updated')
-        return auth.currentUser
-        // ...
-    }).catch((error) => {
-        // An error occurred
-        // ...
-    });
+    admin.auth().getUserByEmail(updateObject.email)
+        .then((userRecord) => {
+            // Update the user's profile information
+            return admin.auth().updateUser(userRecord.uid, updateObject).then((response) => {
+                return res.status(200).send(response);
+            });
+        })
+        .then((error) => {
+            return res.status(404).send(error);
+        })
+        .catch((error) => {
+            console.error('Error updating user profile:', error);
+        });
 };
 const updateUserPassword = async (req: Request, res: Response) => {
     const { newPassword } = req.body
