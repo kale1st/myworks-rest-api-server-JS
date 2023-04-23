8d518c73e098a207970ac18f5d14b220f81af6d3
azizkale <azizkale@hotmail.com>
2023-04-23 12:29:11 +0200

    word edit model updated

--- src/models/editedWord.ts
+++ src/models/editedWord.ts
@@ -1,4 +1,9 @@
 export class EditedWord {
     word: string;
     meaning: string;
+
+    constructor(word: string, meaning: string) {
+        this.word = word;
+        this.meaning = meaning
+    }
 }
