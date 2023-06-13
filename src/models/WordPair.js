3153f69219490a31c807c7490ac6814979978cbd
azizkale <azizkale@hotmail.com>
2023-06-13 21:38:54 +0200

    update

--- src/models/WordPair.js
+++ src/models/WordPair.js
@@ -69,20 +69,6 @@ class WordPair {
                     }
                 });
             });
-            //         let wordpairs: any[] = []
-            //         return await Object.values(chapters).map((data: any) => {
-            //             if (data.wordPairs) {
-            //                 Object.values(data.wordPairs).map((wp: WordPair) => {
-            //                     wordpairs.push(wp)
-            //                 })
-            //             }
-            //         })
-            //     } else {
-            //         return null
-            //     }
-            // }, (error) => {
-            //     return { error: error }
-            // });
         });
     }
     updateWordPair(wordPair) {
